bb4f9cffc080854e4a94039d7c84dd0151fb7361
Sangita Pokhrel <106988913+SangitaPokhrel911@users.noreply.github.com>
2024-01-21 11:06:54 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+##Integrate our code with OpenAI API
+
+import os
+from constants import openai_key
+from langchain.llms import OpenAI
+
+import streamlit as st 
+
+os.environ['OPENAI_API_KEY'] = openai_key
+
+#Streamlit framework 
+
+st.title('Langchain Demo With OpenAI API')
+input_text = st.text_input('Search the topic you want')
+
+
+##OpenAI LLMs
+llm = OpenAI(temperature = 0.8)
+
+
+if input_text:
+    st.write(llm(input_text))
+
